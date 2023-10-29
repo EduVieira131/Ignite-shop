3b0eb5b69bea9cb851ee24f3a941dc20c1eed5c7
EduViera131 <eduviera369@gmail.com>
2023-10-29 16:37:47 -0300

    :feat: stitches added and configurated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$primary100'
+})
+
 export default function Home() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Button>Enviar</Button>
     </>
   )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  css,
+  getCssText,
+  theme,
+  createTheme
+} = createStitches({
+  theme: {
+    colors: {
+      primary100: '#f5f8fa'
+    }
+  }
+})
