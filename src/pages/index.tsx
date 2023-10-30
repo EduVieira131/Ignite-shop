90b95dc3ba84b02c8a68a06858d05388e4b08924
EduViera131 <eduviera369@gmail.com>
2023-10-29 21:19:30 -0300

    :feat: home structure and product card created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
-import { styled } from '../styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green500'
-})
+import camiseta1 from '../assets/camiseta1.png'
+import camiseta2 from '../assets/camiseta2.png'
+import camiseta3 from '../assets/camiseta3.png'
 
 export default function Home() {
   return (
-    <>
-      <Button>Enviar</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
